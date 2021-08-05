fad6d1385790f4848ce4a6038cd6356fe580fa57
Kimaru thagana <macbuntu@localhost.localdomain>
2021-08-05 11:49:22 +0300

    use snowflake provider

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "chanzuckerberg/snowflake"
+      version = "0.25.15"
+    }
+  }
+}
+
+provider "snowflake" {
+  # Configuration options
+}
