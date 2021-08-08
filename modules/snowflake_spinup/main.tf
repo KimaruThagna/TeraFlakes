905726261609a03025d9c389e19966df33bbe657
Kimaru thagana <macbuntu@localhost.localdomain>
2021-08-08 20:53:09 +0300

    setup webserver variables and module

--- modules/snowflake_spinup/main.tf
+++ modules/snowflake_spinup/main.tf
@@ -10,7 +10,7 @@ provider "snowflake" {
 }
 # warehouse
 resource snowflake_warehouse "default_warehouse" {
-  name           = var.warehouse
+  name           = var.warehouse_name
   comment        = "Where analytics happens"
   warehouse_size = var.warehouse_size
 }
@@ -19,22 +19,22 @@ resource "snowflake_database" "transactional_db" {
   name = var.database
 }
 # create role
-resource "snowflake_role" "looker_bi_role" {
+resource "snowflake_role" "db_role" {
   name = var.db_role
 }
 # create user
-resource "snowflake_user" "looker_user" {
+resource "snowflake_user" "db_user" {
   name         = var.db_user
   comment      = "The account Looker will be using to access transactional data for analysis"
   password     = var.db_password
-  default_role = snowflake_role.looker_bi_role.name
+  default_role = snowflake_role.db_role.name
   default_warehouse = snowflake_warehouse.default_warehouse.name
 }
 
 # grant
 resource "snowflake_role_grants" "looker_role_grant" {
-  role_name = snowflake_role.looker_bi_role.name
-  users     = ["ADMIN", snowflake_user.looker_user.name] # list of existing users who can have this role
+  role_name = snowflake_role.db_role.name
+  users     = ["ADMIN", snowflake_user.db_user.name] # list of existing users who can have this role
 }
 
 # create a set of schemas in a single db via a loop
